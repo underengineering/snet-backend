798c274294c520325844aaaba3020b0e79b56079
underengineering <san4a852b@gmail.com>
2023-09-12 08:15:14 +0300

    flow: More secure login

--- src/env-schema.ts
+++ src/env-schema.ts
@@ -2,8 +2,10 @@ import { EnvSchemaOpt, JSONSchemaType } from "env-schema";
 
 type Env = {
     PORT: number;
+    HOSTNAME: string;
     JWT_SECRET: string;
     JWT_EXPIRY_TIME: string;
+    JWT_SECURE: boolean;
     DB_HOST: string;
     DB_PORT: number;
     DB_USERNAME: string;
@@ -21,8 +23,10 @@ export default {
     type: "object",
     required: [
         "PORT",
+        "HOSTNAME",
         "JWT_SECRET",
         "JWT_EXPIRY_TIME",
+        "JWT_SECURE",
         "DB_HOST",
         "DB_PORT",
         "DB_USERNAME",
@@ -30,9 +34,11 @@ export default {
         "DB_DATABASE",
     ],
     properties: {
-        PORT: { type: "string", default: 8080 },
+        PORT: { type: "number", default: 8080 },
+        HOSTNAME: { type: "string", default: "localhost" },
         JWT_SECRET: { type: "string" },
         JWT_EXPIRY_TIME: { type: "string" },
+        JWT_SECURE: { type: "boolean" },
         DB_HOST: { type: "string" },
         DB_PORT: { type: "number", default: 5432 },
         DB_USERNAME: { type: "string" },
--- src/routes/flow/login.ts
+++ src/routes/flow/login.ts
@@ -1,11 +1,21 @@
 import { FastifyInstance } from "fastify";
 import { createHash, timingSafeEqual } from "node:crypto";
 
+import { CookieSerializeOptions } from "@fastify/cookie";
 import { JsonSchemaToTsProvider } from "@fastify/type-provider-json-schema-to-ts";
 
 import { User } from "../../entity/User";
 
 export default async function addRoutes(app: FastifyInstance) {
+    const COOKIE_OPTIONS: CookieSerializeOptions = app.config.JWT_SECURE
+        ? {
+              httpOnly: true,
+              path: "/",
+              sameSite: true,
+              secure: true,
+          }
+        : { path: "/" };
+
     app.withTypeProvider<JsonSchemaToTsProvider>().post(
         "/login",
         {
@@ -48,29 +58,31 @@ export default async function addRoutes(app: FastifyInstance) {
                 email,
             });
 
-            if (foundUser === null)
-                return res.forbidden("Invalid password or email");
-
-            const passwordSalt = Buffer.from(foundUser.passwordSalt, "binary");
-            const passwordSha256 = Buffer.from(
-                foundUser.passwordSha256,
-                "binary"
-            );
+            // Hash against zeros if user is not found to prevent timing attacks
+            const passwordSalt =
+                foundUser === null
+                    ? Buffer.alloc(32)
+                    : Buffer.from(foundUser.passwordSalt, "binary");
+            const passwordSha256 =
+                foundUser === null
+                    ? Buffer.alloc(8)
+                    : Buffer.from(foundUser.passwordSha256, "binary");
 
             const providedPasswordSha256 = createHash("sha256")
                 .update(password)
                 .update(passwordSalt)
                 .digest();
-            if (!timingSafeEqual(providedPasswordSha256, passwordSha256))
+            if (
+                !timingSafeEqual(providedPasswordSha256, passwordSha256) ||
+                foundUser === null
+            )
                 return res.forbidden("Invalid password or email");
 
             const token = await res.jwtSign(
                 { id: foundUser.id },
                 { sign: { expiresIn: app.config.JWT_EXPIRY_TIME } }
             );
-            res.setCookie("token", token, {
-                path: "/",
-            });
+            res.setCookie("token", token, COOKIE_OPTIONS);
 
             return {};
         }
